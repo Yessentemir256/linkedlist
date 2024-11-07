f880267f49941811ca922661473507080955b84e
Yessentemir <yessentemir.utepov@gmail.com>
2024-11-07 17:04:54 +0500

    linked list

--- main.go
+++ main.go
@@ -2,6 +2,49 @@ package main
 
 import "fmt"
 
-func main() {
+// Node представляет узел односвязного списка
+type Node struct {
+	data int
+	next *Node
+}
 
+// LinkedList представляет односвязный список
+type LinkedList struct {
+	head *Node
+}
+
+// Добавление элемента в конец списка
+func (ll *LinkedList) append(data int) {
+	newNode := &Node{data: data}
+
+	if ll.head == nil {
+		ll.head = newNode
+		return
+	}
+
+	current := ll.head
+	for current.next != nil {
+		current = current.next
+	}
+	current.next = newNode
+}
+
+// Вывод содержимого списка
+func (ll *LinkedList) printList() {
+	current := ll.head
+	for current != nil {
+		fmt.Printf("%d -> ", current.data)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
+
+func main() {
+	ll := LinkedList{}
+
+	ll.append(1)
+	ll.append(2)
+	ll.append(3)
+
+	ll.printList()
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 This module was created for learning about singly linked lists and understanding the advantages and disadvantages compared to arrays
+a node is an element in a singly linked list
